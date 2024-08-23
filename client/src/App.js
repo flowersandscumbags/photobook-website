4ea83e1fe83fc9a618e6bcbf660e291ab58fde3a
flowersandscumbags <177429908+flowersandscumbags@users.noreply.github.com>
2024-08-23 03:26:43 -0600

    Update App.js

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Luxe Memories</h1>
+        <p>Your luxury photobook design studio</p>
+      </header>
+      <main>
+        <p>Welcome to our photobook design service. Create beautiful memories today!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
